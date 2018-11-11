bd785338267cc115ff4fc509378a245bc193895a
karendahlaarhus <44139426+karendahlaarhus@users.noreply.github.com>
2018-11-11 14:50:35 +0100

    Update footer.js

--- venziaFERDIG/Prosjekt/footer.js
+++ venziaFERDIG/Prosjekt/footer.js
@@ -2,7 +2,7 @@ footertekst=`
 <footer id="footeren">
   <ul>
      <li class="footer">Tmv-kaia 10, 7042 Trondheim</li>
-     <li class="footer">Mandag - Fredag (11-21) Lørdag (12-22)</li>
+     <li class="footer">Mandag - Fredag (11-21) Lør - Søn (12-22)</li>
      <li class="footer">Telefon: 22 22 55 55 </li>
  </ul>
 </footer>
